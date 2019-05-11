2f4d9a5d88260071fee68134968b1660247db5c0
xhpshamyrus <ruslan.shamyanov@yandex.ru>
2019-05-11 17:06:40 +0500

    Deleted app/static_pages_controller, renamed about.html.rb; author.html.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'static_pages/author'
  # get '/author', to:'static_pages/about#author'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
