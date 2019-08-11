4be467e99fee1ab37cd9cbfb431ba12b246ef6ee
xhpshamyrus <ruslan.shamyanov@yandex.ru>
2019-08-11 14:06:59 +0500

    added dependent destroy to tests(to questions)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
+  has_many :questions, dependent: :destroy
   belongs_to :author, class_name: 'User', optional: true
   has_many :test_passages
   has_many :users, through: :test_passages
